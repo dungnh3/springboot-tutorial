811d78dc43e6fc48ced47f10ad0d280752e73b70
nghoangdung <nguyenhoangdung300993@gmail.com>
2019-08-17 13:53:32 +0700

    Initial Project

---
+++ src/main/java/com/github/springboottutorial/SpringbootTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.github.springboottutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTutorialApplication.class, args);
+	}
+
+}
