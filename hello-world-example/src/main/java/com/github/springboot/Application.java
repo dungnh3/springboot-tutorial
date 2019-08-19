3d133d439ed04c1d6274f897c2eed00ee62db786
nghoangdung <nguyenhoangdung300993@gmail.com>
2019-08-19 10:27:45 +0700

    Initial project Spring REST Hello World Example

---
+++ hello-world-example/src/main/java/com/github/springboot/Application.java
@@ -0,0 +1,17 @@
+package com.github.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dungnh3
+ * @since 19-Aug-2019
+ * @version 0.0.1
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
